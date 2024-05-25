846a69964127386d1eb2d85b4b5c04a6e1d4b813
ayushcodes0 <auyshsingh94@gmail.com>
2024-05-25 11:52:37 +0530

    modified javascript

---
+++ javascriptbasics.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+let a = 5;
+let b = 5;
+let c = "Ayush"
+console.log(a+b);
+console.log(typeof a, typeof b, typeof c); 
+{
+    let a = 55;
+    console.log(a);
+}
+console.log(a);
